e291be8e2eebb7041f3f472388a5c9292301abd6
jjreedv <87283488+jjreedv@users.noreply.github.com>
2021-07-27 19:18:03 -0700

    added more messages and a try/catch block

--- src/ffdfsg/Meat.java
+++ src/ffdfsg/Meat.java
@@ -14,11 +14,11 @@ import org.bukkit.event.EventHandler;
 public class Meat extends JavaPlugin implements CommandExecutor {
 
     public void onEnable() {
-
+        System.out.println("we in");
     }
 
     public void onDisable() {
-
+        System.out.println("we out");
     }
 
     @Override
@@ -32,25 +32,32 @@ public class Meat extends JavaPlugin implements CommandExecutor {
         if (cmd.getName().equalsIgnoreCase("heal")) {
             double maxHealth = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
             p.setHealth(maxHealth);
+            p.sendMessage(ChatColor.RED + "I gotchu");
             return true;
         }
 
         if (cmd.getName().equalsIgnoreCase("feed")) {
             p.setFoodLevel(20);
+            p.sendMessage(ChatColor.RED + "I gotchu");
             return true;
         }
-        if (cmd.getName().equalsIgnoreCase("launch")) {
-            //  is /launch
-            if (args.length == 0) {
+        try {
+            boolean launch = cmd.getName().equalsIgnoreCase("launch");
+            if (launch) {
+                //  is /launch
+                if (args.length == 0) {
+                    p.sendMessage(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "ZOOOOOOOM!");
+                    p.setVelocity(p.getLocation().getDirection().multiply(2).setY(2));
+                    return true;
+                }
+                //  is /launch <number>
                 p.sendMessage(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "ZOOOOOOOM!");
-                p.setVelocity(p.getLocation().getDirection().multiply(2).setY(2));
+                p.setVelocity(p.getLocation().getDirection().multiply(Integer.parseInt(args[0])).setY(2));
                 return true;
             }
-
-            //  is /launch <number>
-            p.sendMessage(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "ZOOOOOOOM!");
-            p.setVelocity(p.getLocation().getDirection().multiply(Integer.parseInt(args[0])).setY(2));
-            return true;
+            return false;
+        } catch (Exception e) {
+            p.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "bruv, numbas only m8");
         }
         return false;
     }
